1e90b44391e32b8b9179ddd35bbef8f45470769f
Andreas Martin <andreas.martin@andreasmartin.ch>
2020-02-25 18:09:56 +0100

    Update

---
+++ src/main/java/ch/fhnw/digibp/config/CsrfAutoConfiguration.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.digibp.config;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CsrfAutoConfiguration {
+    private static final String CSRF_PREVENTION_FILTER = "CsrfPreventionFilter";
+    /**
+     * Overwrite csrf filter from Camunda configured here
+     * org.camunda.bpm.spring.boot.starter.webapp.CamundaBpmWebappInitializer
+     * org.camunda.bpm.spring.boot.starter.webapp.filter.SpringBootCsrfPreventionFilter
+     * Is configured with basically a 'no-op' filter
+     */
+    @Bean
+    public ServletContextInitializer csrfOverwrite() {
+        return servletContext -> servletContext.addFilter(CSRF_PREVENTION_FILTER, (request, response, chain) -> chain.doFilter(request, response));
+    }
+}
